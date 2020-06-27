517bdb46de8398cead61e21edf1a205c24e13960
bangbang6 <32785707+bangbang6@users.noreply.github.com>
2020-06-27 09:50:50 +0800

    init

---
+++ test.js
@@ -0,0 +1,5 @@
+function greeter1(person) {
+    return "Hello, " + person;
+}
+var user1= "Jane User";
+console.log(greeter1(user1));
---
+++ test.ts
@@ -0,0 +1,7 @@
+function greeter1(person:string) :string {
+    return "Hello, " + person;
+}
+
+let user1 = "Jane User";
+
+console.log(greeter1(user1))
